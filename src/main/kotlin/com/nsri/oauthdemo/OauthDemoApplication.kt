961a43a7fd4d1047bef38cc94df327f9e55ba93f
Neha Srivastava <6138253+neha-sri@users.noreply.github.com>
2023-04-15 18:19:34 -0400

    Minor updates

--- src/main/kotlin/com/nsri/oauthdemo/OauthDemoApplication.kt
+++ src/main/kotlin/com/nsri/oauthdemo/OauthDemoApplication.kt
@@ -2,7 +2,6 @@ package com.nsri.oauthdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 /**
  * @author neha-sri
  */
